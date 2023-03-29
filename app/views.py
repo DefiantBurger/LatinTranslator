007999acf372a60db8fe5bd3196ef169366126ad
DefiantBurger <s018041@students.lmsd.org>
2023-03-29 00:11:25 -0400

    Integrated http://latindictionary.wikidot.com features

--- app/views.py
+++ app/views.py
@@ -3,6 +3,16 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from flask import Blueprint, render_template, request
+import re
+
+
+pos_to_full = {
+	"N": "noun",
+	"V": "verb",
+	"ADJ": "adjective",
+	"PREP": "preposition"
+}
+
 
 views = Blueprint('views', __name__)
 
@@ -28,4 +38,26 @@ def post_latin():
 	translation = soup.find('pre')
 	translation = translation.decode_contents()
 
-	return "idkwhattoputhere", 200, {"word": word, "translation": translation}
+	parts = []
+	lines = list(filter(None, translation.split("<br/>")))
+	for ln in lines:
+		matches = re.findall(r"(?<!\S)[A-Z]+(?!\S)", ln)
+		if matches:
+			parts.append(matches[0])
+
+	parts = sorted(list(set(parts)))
+
+	checked_parts = []
+	for part in parts:
+		code = requests.head(f"http://latindictionary.wikidot.com/{pos_to_full[part]}:{word}").status_code
+		print(code)
+		if code == 404:
+			checked_parts.append(f"{part}=0")
+		else:
+			checked_parts.append(f"{part}=1")
+
+	return "idkwhattoputhere", 200, {"word": word, "translation": translation, "parts": ",".join(checked_parts)}
+
+@views.route('/report-issue')
+def report_issue():
+	pass
