71cb88b9025b9be0e5eab9ffbd9beba524767bae
DefiantBurger <s018041@students.lmsd.org>
2023-04-25 17:14:46 -0400

    Fixed Latin-to-English translation, still need to fix English-to-Latin translation.

--- app/views.py
+++ app/views.py
@@ -3,8 +3,6 @@ import json
 import requests
 from bs4 import BeautifulSoup
 from flask import Blueprint, render_template, request
-import re
-
 
 pos_to_full = {
 	"N": "noun",
@@ -12,7 +10,6 @@ pos_to_full = {
 	"ADJ": "adjective",
 	"PREP": "preposition"
 }
-
 
 views = Blueprint('views', __name__)
 
@@ -26,37 +23,32 @@ def get_latin():
 def post_latin():
 	post_data = json.loads(request.data.decode())
 	lang = post_data[0]
-	if lang == "latin":
-		lang = "keyword"
+	if lang != "latin":
+		return "idkwhattoputhere", 200, {"word": "Error", "translation": "-----English-To-Latin translation is temporarily disabled, sorry."}
 	word = post_data[1]
-	resp = requests.get(f"https://archives.nd.edu/cgi-bin/wordz.pl?{lang}={word}", timeout=5)
-	html = resp.content.decode()
-	html = html.replace("\n", "<br>")
-	soup = BeautifulSoup(html, 'html.parser')
+	translation = requests.get(f"https://latin-words.com/cgi-bin/translate.cgi?query={word}", timeout=5).json()["message"]
+	translation = translation.replace("\n", "<br>").replace("\r", "")
 
-	word = soup.find('h2').text
-	translation = soup.find('pre')
-	translation = translation.decode_contents()
+	# parts = []
+	# lines = list(filter(None, translation.split("<br/>")))
+	# for ln in lines:
+	# 	matches = re.findall(r"(?<!\S)[A-Z]+(?!\S)", ln)
+	# 	if matches:
+	# 		parts.append(matches[0])
+	#
+	# parts = sorted(list(set(parts)))
+	#
+	# checked_parts = []
+	# for part in parts:
+	# 	code = requests.head(f"http://latindictionary.wikidot.com/{pos_to_full[part]}:{word}").status_code
+	# 	print(code)
+	# 	if code == 404:
+	# 		checked_parts.append(f"{part}=0")
+	# 	else:
+	# 		checked_parts.append(f"{part}=1")
 
-	parts = []
-	lines = list(filter(None, translation.split("<br/>")))
-	for ln in lines:
-		matches = re.findall(r"(?<!\S)[A-Z]+(?!\S)", ln)
-		if matches:
-			parts.append(matches[0])
+	return "idkwhattoputhere", 200, {"word": word, "translation": translation}
 
-	parts = sorted(list(set(parts)))
-
-	checked_parts = []
-	for part in parts:
-		code = requests.head(f"http://latindictionary.wikidot.com/{pos_to_full[part]}:{word}").status_code
-		print(code)
-		if code == 404:
-			checked_parts.append(f"{part}=0")
-		else:
-			checked_parts.append(f"{part}=1")
-
-	return "idkwhattoputhere", 200, {"word": word, "translation": translation, "parts": ",".join(checked_parts)}
 
 @views.route('/report-issue')
 def report_issue():
